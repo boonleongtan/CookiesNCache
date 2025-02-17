d2dc45abeb4b9b5edbee796d8f9ded6fe28d0afa
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-17 14:58:55 +0000

    delete space lol

--- react-flask/flask-api/api.py
+++ react-flask/flask-api/api.py
@@ -8,7 +8,6 @@
 #     return {'time': time.time()}
 
 import sqlite3
-
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
