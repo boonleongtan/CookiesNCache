55c4a761d53ab7945e28221fc54ae3128a7b5f84
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-17 11:07:28 +0000

    make app same as rest; reset app.css to default but dont use

--- cookies-and-cache/src/App.js
+++ cookies-and-cache/src/App.js
@@ -1,12 +1,10 @@
 import Layout from './components/Layout';
-import './App.css';
+// import './App.css';
 
 function App() {
     return (
         <Layout>
-            <div className="App">
-                <p>We all start from somewhere</p>
-            </div>
+            <h1>Products</h1>
         </Layout>
     );
 }
