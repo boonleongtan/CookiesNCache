f815ea696125f90b9933c88539d5adcecd1c42ae
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-17 10:09:58 +0000

    update navbar 1

--- cookies-and-cache/src/components/Navbar.js
+++ cookies-and-cache/src/components/Navbar.js
@@ -3,37 +3,37 @@ import './Navbar.css';
 
 function Navbar() {
     return (
-        <nav class="navbar">
-            <ul class="nav-list">
+        <nav className="navbar">
+            <ul className="nav-list">
             {/* <!--dropdown--> */}
-                <li class="nav-item"><Link to="{/about}">
-                    <div class="dropdown">
-                        <button class="dropdown-button">
+                <li className="nav-item"><Link to="{/about}">
+                    <div className="dropdown">
+                        <button className="dropdown-button">
                             <a href="/">SHOP NOW</a>
-                            <span class="caret-down"></span>
+                            <span className="caret-down"></span>
                         </button>
-                        <div class="dropdown-content">
+                        <div className="dropdown-content">
                             <a href="/">ALL PRODUCTS</a>
                             <a href="/favs">ALL-TIME FAVOURITES</a>
                         </div>
                     </div>
                     </Link></li>
             {/* <!--normal nav-item--> */}
-                <li class="nav-item"><a class="nav-link" href="/seasonal">SEASONAL DELIGHTS</a></li>
+                <li className="nav-item"><a className="nav-link" href="/seasonal">SEASONAL DELIGHTS</a></li>
             {/* <!--normal nav-item--> */}
-                <li class="nav-item"><a class="nav-link" href="/about">ABOUT</a></li>
+                <li className="nav-item"><a className="nav-link" href="/about">ABOUT</a></li>
             {/* <!--search bar--> */}
-                <li class="nav-item">
-                    <div class="search">
+                <li className="nav-item">
+                    <div className="search">
                         <input type="text" name="search" id="nav-searchbar" placeholder="Search for a product" onkeydown="search()" />
-                        <button class="nav-searchbutton">Search</button>
-                        <div class="nav-searchlist"></div>
+                        <button className="nav-searchbutton">Search</button>
+                        <div className="nav-searchlist"></div>
                     </div>
                 </li>
             {/* <!--profile icon--> */}
-                <li class="nav-item"><a class="nav-profile" href="/profile"><img src="/static/profile_icon.jpg" alt="Profile" class="nav-profile" /></a></li>
+                <li className="nav-item"><a className="nav-profile" href="/profile"><img src="/static/profile_icon.jpg" alt="Profile" className="nav-profile" /></a></li>
             {/* <!--cart icon--> */}
-                <li class="nav-item"><a class="nav-profile" href="/cart"><img src="/static/cart_icon.png" alt="Cart" class="nav-cart" /></a></li>
+                <li className="nav-item"><a className="nav-profile" href="/cart"><img src="/static/cart_icon.png" alt="Cart" className="nav-cart" /></a></li>
             </ul>
         </nav>
     );
