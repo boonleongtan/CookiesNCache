5ec74587938a4171950835a7f5b084abe8b5b951
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-17 09:30:39 +0000

    create layout template

--- cookies-and-cache/src/App.js
+++ cookies-and-cache/src/App.js
@@ -1,21 +1,20 @@
 import logo from './cookies.jpg';
-import Navbar from './components/Navbar.js'
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            We all start from somewhere
-          </p>
-        </header>
-      </div>
+      <Layout>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              We all start from somewhere
+            </p>
+          </header>
+        </div>
+      </Layout>
     </>
   );
 }
--- cookies-and-cache/src/components/Layout.js
+++ cookies-and-cache/src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from './Navbar';
 
-function Layout() {
+function Layout({ children }) {
     return (
         <div>
             <Navbar />
+            {children}
         </div>
     )
 }
