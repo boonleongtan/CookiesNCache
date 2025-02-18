89363f2a1e6acde93bd0096d9cf1b67f7bbe9aac
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-18 00:52:32 +0000

    remove product.js

--- react-flask/react-client/src/App.js
+++ react-flask/react-client/src/App.js
@@ -1,21 +1,12 @@
-// import { useState, useEffect } from 'react';
-import Products from './components/Products';
+import Grid from './components/Grid';
 // import './App.css';
 
 function App() {
-    // const [currentTime, setCurrentTime] = useState(0);
-
-    // useEffect(() => {
-    //     fetch('/api/time').then(res => res.json()).then(data => {
-    //         setCurrentTime(data.time);
-    //     });
-    // }, []);
 
     return (
         <>
             <h1>Products</h1>
-            {/* <p>The current time is {currentTime}</p> */}
-            <Products />
+            <Grid apiRoute="/api/" />
         </>
     );
 }
--- react-flask/react-client/src/components/Grid.js
+++ react-flask/react-client/src/components/Grid.js
@@ -1,11 +1,38 @@
+import { useState, useEffect } from 'react';
 import Layout from './Layout';
-import './Grid.css'
+import './Grid.css';
 
-function Grid({ children }) {
+function Grid({ apiRoute, children }) {
+    const [cookieList, setCookieList] = useState([]);
+
+    useEffect(() => {
+        fetch(apiRoute).then(res => res.json()).then(data => {
+            setCookieList(data);
+        });
+    }, []);
+
     return (
         <Layout>
             <div className="grid-container">
                 {children}
+                <form>
+                    <input name="id" type="hidden" value={cookieList[0].id} />
+                    <button className="grid-item" type="submit">
+                        <img src={cookieList[0].img} alt={"Image of " + cookieList[0].name} className="item-img" />
+                        <div className="item-name">{cookieList[0].name}</div>
+                        <div className="item-value">{cookieList[0].price}</div>
+                    </button>
+                </form>
+                {/* {% for cookie in cookies %}
+                    <form action="/product" method="post">
+                        <input name="id" type="hidden" value="{{ cookie.id }}">
+                        <button class="grid-item" type="submit">
+                            <img src="{{ cookie.img }}" alt="Image of {{ cookie.name }}" class="item-img">
+                            <div class="item-name">{{ cookie.name }}</div>
+                            <div class="item-value">{{ cookie.price | usd }}</div>
+                        </button>
+                    </form>
+                {% endfor %} */}
             </div>
         </Layout>
     )
