2c073fe4000c33171e291e7caed321ee349f7897
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-18 03:07:31 +0000

    edit

--- react-flask/react-client/src/components/Grid.js
+++ react-flask/react-client/src/components/Grid.js
@@ -31,16 +31,6 @@ function Grid({ apiRoute, children }) {
                 <form>
                     {gridItems}
                 </form>
-                {/* {% for cookie in cookies %}
-                    <form action="/product" method="post">
-                        <input name="id" type="hidden" value="{{ cookie.id }}">
-                        <button class="grid-item" type="submit">
-                            <img src="{{ cookie.img }}" alt="Image of {{ cookie.name }}" class="item-img">
-                            <div class="item-name">{{ cookie.name }}</div>
-                            <div class="item-value">{{ cookie.price | usd }}</div>
-                        </button>
-                    </form>
-                {% endfor %} */}
             </div>
         </Layout>
     )
