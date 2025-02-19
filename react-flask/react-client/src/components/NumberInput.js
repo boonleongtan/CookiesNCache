de77437da4d659a404bf217d1fb0cec7229c81e2
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-19 14:37:00 +0000

    update number input

--- react-flask/react-client/src/components/NumberInput.js
+++ react-flask/react-client/src/components/NumberInput.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './NumberInput.css';
 
 function NumberInput() {
+    const [qty, setQty] = useState(1);
+
+    function incCnt(e) {
+        e.preventDefault();
+        setQty(qty => qty - 1);
+    }
+    
+    function decCnt(e) {
+        e.preventDefault();
+        setQty(qty => qty + 1);
+    }
+
     return (
         <div className="number-input">
-            <button onclick="this.parentNode.querySelector('input[type=number]').stepDown();event.preventDefault();" type="button"></button>
-            <input name="qty" type="number" min="1" value="1" />
-            <button onclick="this.parentNode.querySelector('input[type=number]').stepUp();event.preventDefault();" className="plus" type="button"></button>
+            {/* set type="button" to prevent submit */}
+            <button onClick={decCnt} type="button"></button>
+            <input name="qty" type="number" min="1" value={qty} />
+            <button onClick={incCnt} className="plus" type="button"></button>
         </div>
     );
 }
