28d5f379759ec7e3884efaf0c89551f39cdbbe51
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-19 05:08:36 +0000

    update profile.js

--- react-flask/react-client/src/pages/profile.js
+++ react-flask/react-client/src/pages/profile.js
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
 import Layout from '../components/Layout';
 
-function Profile() {
+function Profile({ username = "user" }) {
     useEffect(() => {
         document.title = 'Profile | Cookies & Cache!';
     }, []);
 
     return (
         <Layout>
-            <h1>Profile</h1>
+            <h1>Welcome, {username}</h1>
+
+            <h4 style={{marginTop: 0}}>Any item you add to cart now will automatically be saved for your next purchase, unless you checkout.</h4>
+
+            <form action="/logout" className="login">
+                <button type="submit">Log out</button>
+            </form>
         </Layout>
     );
 }
