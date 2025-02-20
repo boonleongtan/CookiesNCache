36df7626f3297576ccd5c0817287431ef6d781dc
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-20 18:26:27 +0800

    map cookie row

--- react-flask/react-client/src/pages/fullcart.js
+++ react-flask/react-client/src/pages/fullcart.js
@@ -1,5 +1,43 @@
 function FullCart({ cookieCart }) {
-    console.log(cookieCart);
+    console.log("Successfully received cart data: " + cookieCart);
+
+    const cookieRow = cookieCart[0] && cookieCart.map((cookie) => {
+        return (
+            <tr>
+                <td>
+                    <img
+                        src={cookie.img}
+                        alt={cookie.name}
+                        className="cart-item-left cart-img"
+                    />
+                    <div className="cart-item-right">
+                        <form>
+                            <input
+                                name="id"
+                                type="hidden"
+                                value={cookie.id}
+                            />
+                            <button href="/product">{cookie.name}</button>
+                        </form>
+                        <p>{cookie.price}</p>
+                    </div>
+                </td>
+                <td>
+                    TODO
+                    {/* <form action="/editcart" method="post">
+                        <input name="id" type="hidden" value="{{ cookie.id }}">
+                        <div className="number-input">
+                            <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"></button>
+                            <input name="qty" className="qty-indicator" type="number" min="0" value="{{ cookie.qty }}">
+                            <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" className="plus"></button>
+                        </div>
+                    </form> */}
+                </td>
+                <td>{cookie.total}</td>
+            </tr>
+        );
+    })
+
     return (
         <>
             <table className="cart-table">
@@ -10,38 +48,7 @@ function FullCart({ cookieCart }) {
                     <th>TOTAL</th>
                 </tr>
 
-                    <tr>
-                        <td>
-                            <img
-                                src={cookieCart[0].img}
-                                alt={cookieCart[0].name}
-                                className="cart-item-left cart-img"
-                            />
-                            <div className="cart-item-right">
-                                <form>
-                                    <input
-                                        name="id"
-                                        type="hidden"
-                                        value={cookieCart[0].id}
-                                    />
-                                    <button href="/product">{cookieCart[0].name}</button>
-                                </form>
-                                <p>{cookieCart[0].price}</p>
-                            </div>
-                        </td>
-                        <td>
-                            TODO
-                            {/* <form action="/editcart" method="post">
-                                <input name="id" type="hidden" value="{{ cookie.id }}">
-                                <div className="number-input">
-                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"></button>
-                                    <input name="qty" className="qty-indicator" type="number" min="0" value="{{ cookie.qty }}">
-                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" className="plus"></button>
-                                </div>
-                            </form> */}
-                        </td>
-                        <td>{cookieCart[0].total}</td>
-                    </tr>
+                {cookieRow}
 
                 {/* <tr>
                     <td></td>
