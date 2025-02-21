9434bd38e45d9d01d3cae1dbfc63a2b7d60d3b08
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-21 22:51:56 +0800

    update state name to better reflect type

--- react-flask/react-client/src/pages/fullcart.js
+++ react-flask/react-client/src/pages/fullcart.js
@@ -6,8 +6,8 @@ import './fullcart.css';
 function FullCart({ cookieCart }) {
     // console.log("Successfully received cart data: ");
     // console.log(cookieCart);
-    const [rcvdQtyDict, setRcvdQtyDict] = useState({});
-    // console.log(rcvdQtyDict);
+    const [rcvdQtysObj, setRcvdQtysObj] = useState({});
+    // console.log(rcvdQtysObj);
 
     async function sendUpdatedCart() {
         const response = await fetch("/api/cart", {
@@ -16,7 +16,7 @@ function FullCart({ cookieCart }) {
                 'Accept': 'application/json',
                 'Content-type': 'application/json',
             },
-            body: JSON.stringify(rcvdQtyDict),
+            body: JSON.stringify(rcvdQtysObj),
         });
         if (response.ok) {
             console.log('Cart updated successfully');
@@ -25,7 +25,7 @@ function FullCart({ cookieCart }) {
 
     useEffect(() => {
         sendUpdatedCart();
-    }, [rcvdQtyDict]);
+    }, [rcvdQtysObj]);
 
 
     const cookieRows = cookieCart.cookierows && cookieCart.cookierows.map((cookie) => {
@@ -43,7 +43,7 @@ function FullCart({ cookieCart }) {
                     </div>
                 </td>
                 <td>
-                    <NumberInput initialValue={cookie.qty} sendDataToParent={data => {setRcvdQtyDict({...rcvdQtyDict, [cookie.id]: data});console.log('works')}} />
+                    <NumberInput initialValue={cookie.qty} sendDataToParent={data => {setRcvdQtysObj({...rcvdQtysObj, [cookie.id]: data});console.log('works')}} />
                 </td>
                 <td>{cookie.total}</td>
             </tr>
