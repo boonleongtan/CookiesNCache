04dbae7a421ec9c4f808327411570df5e98211ce
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-02-27 11:02:30 +0800

    change to footer only and add tbody

--- react-flask/react-client/src/pages/checkout.js
+++ react-flask/react-client/src/pages/checkout.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Layout from '../components/Layout';
+import Footer from '../components/Footer';
 import './checkout.css';
 
 function Checkout() {
@@ -7,71 +7,71 @@ function Checkout() {
         <>
             <title>Checkout</title>
 
-            <Layout>
-                <header className="special-header">
-                    <Link to="/">Cookies & Cache</Link>
-                </header>
+            <header className="special-header">
+                <Link to="/">Cookies & Cache</Link>
+            </header>
 
-                <h1>Delivery</h1>
+            <h1>Delivery</h1>
 
-                <div className="vert-split-half">
+            <div className="vert-split-half">
 
-                {/* <!--Left half: fill in details sections--> */}
-                    <div className="each-half" style={{width:'60%'}}>
-                        <form action="/checkout" method="post">
-                            <div className="fill-in-details-section">
-                                <h3>Contact</h3>
-                                <input autocomplete="email" autofocus name="email" placeholder="Email" type="email" required />
-                                <input autocomplete="tel" name="phone-no" inputmode="numeric" pattern="\+?[0-9\-\s]+" title="Allowed characters: +, -, space, numbers" placeholder="Phone Number" type="tel" required />
+            {/* <!--Left half: fill in details sections--> */}
+                <div className="each-half" style={{width:'60%'}}>
+                    <form action="/checkout" method="post">
+                        <div className="fill-in-details-section">
+                            <h3>Contact</h3>
+                            <input autocomplete="email" autofocus name="email" placeholder="Email" type="email" required />
+                            <input autocomplete="tel" name="phone-no" inputmode="numeric" pattern="\+?[0-9\-\s]+" title="Allowed characters: +, -, space, numbers" placeholder="Phone Number" type="tel" required />
+                        </div>
+                        <div className="fill-in-details-section">
+                            <h3>Delivery Address</h3>
+                            <label for="country">Country/Region</label>
+                            <select name="country" style={{width:'95%'}} required>
+                                <option value="" disabled>Select Country/Region</option>
+                                <option value="Singapore" selected>Singapore</option>
+                            </select>
+                            <div className="fill-in-details-inline">
+                                <input autocomplete="given-name" name="fname" placeholder="First Name" type="text" required />
+                                <input autocomplete="family-name" name="lname" placeholder="Last Name" type="text" required />
                             </div>
-                            <div className="fill-in-details-section">
-                                <h3>Delivery Address</h3>
-                                <label for="country">Country/Region</label>
-                                <select name="country" style={{width:'95%'}} required>
-                                    <option value="" disabled>Select Country/Region</option>
-                                    <option value="Singapore" selected>Singapore</option>
-                                </select>
-                                <div className="fill-in-details-inline">
-                                    <input autocomplete="given-name" name="fname" placeholder="First Name" type="text" required />
-                                    <input autocomplete="family-name" name="lname" placeholder="Last Name" type="text" required />
-                                </div>
-                                <input autocomplete="street-address" name="address" placeholder="Address" type="text" required />
-                                <input autocomplete="postal-code" name="postal-code" placeholder="Postal Code" type="text" required />
+                            <input autocomplete="street-address" name="address" placeholder="Address" type="text" required />
+                            <input autocomplete="postal-code" name="postal-code" placeholder="Postal Code" type="text" required />
+                        </div>
+                        <div className="fill-in-details-section">
+                            <h3>Delivery Date and Time</h3>
+                            <input name="delivery-datetime" type="datetime-local" required />
+                        </div>
+                        <div className="fill-in-details-section">
+                            <h3>Payment</h3>
+                            <input autocomplete="cc-number" name="card-no" inputmode="numeric" maxlength="19" pattern="[0-9\s]{13,19}" placeholder="Card Number" title="Must be a valid credit card number!" type="tel" required />
+                            <div className="fill-in-details-inline">
+                                <input autocomplete="cc-exp" name="card-exp" placeholder="Expiration Date (MM/YY)" pattern="(0[1-9]|1[0-2])/[0-9]{2}" title="MM/YY" type="text" required />
+                                <input autocomplete="cc-csc" name="card-code" inputmode="numeric" maxlength="3" pattern="[0-9]{3}" placeholder="CVV" title="xxx" type="tel" required />
                             </div>
-                            <div className="fill-in-details-section">
-                                <h3>Delivery Date and Time</h3>
-                                <input name="delivery-datetime" type="datetime-local" required />
-                            </div>
-                            <div className="fill-in-details-section">
-                                <h3>Payment</h3>
-                                <input autocomplete="cc-number" name="card-no" inputmode="numeric" maxlength="19" pattern="[0-9\s]{13,19}" placeholder="Card Number" title="Must be a valid credit card number!" type="tel" required />
-                                <div className="fill-in-details-inline">
-                                    <input autocomplete="cc-exp" name="card-exp" placeholder="Expiration Date (MM/YY)" pattern="(0[1-9]|1[0-2])/[0-9]{2}" title="MM/YY" type="text" required />
-                                    <input autocomplete="cc-csc" name="card-code" inputmode="numeric" maxlength="3" pattern="[0-9]{3}" placeholder="CVV" title="xxx" type="tel" required />
-                                </div>
-                                <input autocomplete="cc-name" name="card-name" placeholder="Name on card" type="text" required />
-                            </div>
-                            <input name="prediscount" type="hidden" value="{{ subtotal | usd }}" />
-                            <input name="paid" type="hidden" value="{{ total | usd }}" />
-                            <button className="paynow" type="submit">Pay now</button>
-                        </form>
-                    </div>
+                            <input autocomplete="cc-name" name="card-name" placeholder="Name on card" type="text" required />
+                        </div>
+                        <input name="prediscount" type="hidden" value="{{ subtotal | usd }}" />
+                        <input name="paid" type="hidden" value="{{ total | usd }}" />
+                        <button className="paynow" type="submit">Pay now</button>
+                    </form>
+                </div>
 
-                {/* <!--Right half: checkout product details--> */}
-                    <div className="each-half checkout-info-section">
-                        <table>
+            {/* <!--Right half: checkout product details--> */}
+                <div className="each-half checkout-info-section">
+                    <table>
+                        <tbody>
 
                         {/* <!--product row--> */}
                             {/* {% for cookie in cookies %}
                                 <tr>
-                                    <td>
-                                        <img src="{{ cookie.img }}" className="checkout-img">
-                                        <p>{{ cookie.name }}</p>
-                                    </td>
-                                    <td>{{ cookie.qty }}</td>
-                                    <td>{{ cookie.total | usd }}</td>
+                                <td>
+                                <img src="{{ cookie.img }}" className="checkout-img">
+                                <p>{{ cookie.name }}</p>
+                                </td>
+                                <td>{{ cookie.qty }}</td>
+                                <td>{{ cookie.total | usd }}</td>
                                 </tr>
-                            {% endfor %} */}
+                                {% endfor %} */}
 
                         {/* <!--subtotal row--> */}
                             <tr className="no-border-bottom no-padding">
@@ -113,12 +113,13 @@ function Checkout() {
                                 </td>
                             </tr>
 
-                        </table>
-                    </div>
-
+                        </tbody>
+                    </table>
                 </div>
 
-            </Layout>
+            </div>
+
+            <Footer />
         </>
     );
 }
