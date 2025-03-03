68b0635ecfafae53b5ba4c5dfaf6b428e95e0c36
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-03-03 17:05:58 +0800

    change the way to login

--- react-flask/react-client/src/pages/profile.js
+++ react-flask/react-client/src/pages/profile.js
@@ -1,24 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Layout from '../components/Layout';
-import Login from './login';
 import './login.css';
 
-function Profile({ username = "user" }) {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+function Profile() {
+    let navigate = useNavigate();
+
+    const [user, setUser] = useState(false);
+
+    useEffect(() => {
+        fetch('/api/login').then(res => res.json()).then(data => setUser(data));
+    }, []);
 
     function handleLogOut() {
-        setIsLoggedIn(false);
+        setUser(false);
     }
 
-    if (!isLoggedIn) {
-        return <Login setIsLoggedIn={setIsLoggedIn} />;
+    if (!user) {
+        navigate('/Login', { replace: true });
     } else {
         return (
             <>
                 <title>Profile | Cookies & Cache!</title>
     
                 <Layout>
-                    <h1>Welcome, {username}</h1>
+                    <h1>Welcome, {user}</h1>
                     <h4 style={{marginTop:0}}>Any item you add to cart now will automatically be saved for your next purchase, unless you checkout.</h4>
                     <div className="login"><button type="submit" onClick={handleLogOut}>Log out</button></div>
                 </Layout>
