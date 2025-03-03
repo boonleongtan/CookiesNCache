bccd65fc3cf9a9a402c85a5618b7b95c53ffe1b4
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-03-03 16:49:27 +0800

    add registration page

--- react-flask/react-client/src/pages/login.js
+++ react-flask/react-client/src/pages/login.js
@@ -1,34 +1,44 @@
+import { useState } from 'react';
 import Layout from '../components/Layout';
 import './login.css';
+import Register from './register';
 
 function Login({ setIsLoggedIn }) {
+    const [isRegistered, setIsRegistered] = useState(true);
+
     function handleLogIn() {
         setIsLoggedIn(true);
     }
+
+    function handleRegister() {
+        setIsRegistered(false);
+    }
+
+    if (!isRegistered) {
+        return <Register setIsRegistered={setIsRegistered} />;
+    } else {
+        return (
+            <>
+                <title>Login | Cookies & Cache!</title>
     
-    return (
-        <>
-            <title>Login | Cookies & Cache!</title>
-
-            <Layout>
-                <h1 style={{marginBottom:'25px'}}>Login</h1>
-                <h4 style={{marginTop:'-20px'}}>Sign in now to gain access to your own shopping cart!</h4>
-                <h4 style={{color:'red',backgroundColor:'yellow',marginBottom:'25px'}}>WARNING! ALL YOUR CURRENT CART ITEMS WILL BE ADDED TO YOUR PERSONAL SHOPPING CART AND SAVED TO CLOUD. PROCEED WITH CAUTION.</h4>
-
-                <div className="login">
-                    <form action="/login" method="post">
-                        <input autocomplete="off" autofocus name="username" placeholder="Username" type="text" />
-                        <input name="password" placeholder="Password" type="password" />
-                        <button type="submit" onClick={handleLogIn}>Log In</button>
-                    </form>
-                    <span>---------- <i>or</i> ----------</span>
-                    <form action="/register" method="get">
-                        <button type="submit" style={{paddingLeft:'105px',paddingRight:'105px'}}>Sign Up</button>
-                    </form>
-                </div>
-            </Layout>
-        </>
-    );
+                <Layout>
+                    <h1 style={{marginBottom:'25px'}}>Login</h1>
+                    <h4 style={{marginTop:'-20px'}}>Sign in now to gain access to your own shopping cart!</h4>
+                    <h4 style={{color:'red',backgroundColor:'yellow',marginBottom:'25px'}}>WARNING! ALL YOUR CURRENT CART ITEMS WILL BE ADDED TO YOUR PERSONAL SHOPPING CART AND SAVED TO CLOUD. PROCEED WITH CAUTION.</h4>
+    
+                    <div className="login">
+                        <form action={handleLogIn}>
+                            <input autocomplete="off" autofocus name="username" placeholder="Username" type="text" />
+                            <input name="password" placeholder="Password" type="password" />
+                            <button type="submit">Log In</button>
+                        </form>
+                        <span>---------- <i>or</i> ----------</span>
+                        <button type="submit" style={{paddingLeft:'105px',paddingRight:'105px'}} onClick={handleRegister}>Sign Up</button>
+                    </div>
+                </Layout>
+            </>
+        );
+    }
 }
 
 export default Login;
---
+++ react-flask/react-client/src/pages/register.js
@@ -0,0 +1,28 @@
+import Layout from '../components/Layout';
+import './login.css';
+
+function Register({ setIsRegistered }) {
+    function handleRegister() {
+        setIsRegistered(true);
+    }
+
+    return (
+        <>
+            <title>Register | Cookies & Cache!</title>
+
+            <Layout>
+                <h1 style={{marginBottom:'25px'}}>Register</h1>
+
+                <form action={handleRegister} className="login">
+                    {/* <!--User should be prompted for username, password, and a confirmation--> */}
+                    <input autocomplete="off" autofocus name="username" placeholder="Username" type="text" />
+                    <input name="password" placeholder="Password" type="password" />
+                    <input name="confirmation" placeholder="Password (again)" type="password" />
+                    <button type="submit">Register</button>
+                </form>
+            </Layout>
+        </>
+    );
+}
+
+export default Register;
