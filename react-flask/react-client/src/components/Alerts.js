58d193aa5a95abb631bef4fc793c27e8a88e00a8
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-03-04 19:49:42 +0800

    add to alerts.js

--- react-flask/react-client/src/components/Alerts.js
+++ react-flask/react-client/src/components/Alerts.js
@@ -1,8 +1,20 @@
 import './Alerts.css';
 
 function AddToCartAlert() {
+    function overlayOff() {}
+
     return (
-        <></>
+        <div className="overlay" onClick={overlayOff}>
+            <div className="alert" style="height:50%;">
+            {/* <!--use cross symbol to close alert--> */}
+                <span className="close-alert" onclick="this.parentElement.style.display='none';overlay_off()">&times;</span>
+            {/* <!--alert message--> */}
+                <p style="color:blue;"></p>
+            {/* <!--nav buttons--> */}
+                <a href="/" id="left">Shop all products {'>'}</a>
+                <a href="/cart" id="right">View Cart {'>'}</a>
+            </div>
+        </div>
     );
 }
 
