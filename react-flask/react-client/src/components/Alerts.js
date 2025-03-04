46ee09b983522de65b85d72ce3dbb779d76a882d
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2025-03-04 20:14:54 +0800

    html to jsx

--- react-flask/react-client/src/components/Alerts.js
+++ react-flask/react-client/src/components/Alerts.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import './Alerts.css';
 
-export function AddToCartAlert() {
-    function overlayOff() {
-        document.querySelector(".overlay").style.display = "none";
-    }
+function overlayOff() {
+    document.querySelector(".overlay").style.display = "none";
+}
 
+export function AddToCartAlert() {
     return (
         <div className="overlay" onClick={overlayOff}>
             <div className="alert" style={{height:'50%'}}>
@@ -16,6 +16,32 @@ export function AddToCartAlert() {
             {/* <!--nav buttons--> */}
                 <Link to="/" id="left">Shop all products {'>'}</Link>
                 <Link to="/Cart" id="right">View Cart {'>'}</Link>
+            </div>
+        </div>
+    );
+}
+
+export function LoginErrorMsgs() {
+    return (
+        <div className="overlay" onClick={overlayOff}>
+            <div className="alert" style={{height:'40%'}}>
+            {/* <!--use cross symbol to close alert--> */}
+                <span className="close-alert" onClick={overlayOff}>&times;</span>
+            {/* <!--alert message--> */}
+                <p style={{color:'red'}}>???</p>
+            </div>
+        </div>
+    );
+}
+
+export function CheckoutAlert() {
+    return (
+        <div className="overlay" onClick={overlayOff}>
+            <div className="alert" style={{height:'40%'}}>
+            {/* <!--use cross symbol to close alert--> */}
+                <span className="close-alert" onClick={overlayOff}>&times;</span>
+            {/* <!--alert message--> */}
+                <p style={{color:'green'}}></p>
             </div>
         </div>
     );
